c19fb8e897cb9a47de8d389e51ceff95020031c2
MT <towfeagl2024@gmail.com>
2026-04-20 01:03:53 +0200

    Add canonical dealer directory redirects

--- next.config.js
+++ next.config.js
@@ -5,6 +5,30 @@ const nextConfig = {
   turbopack: {
     root: path.join(__dirname),
   },
+  async redirects() {
+    return [
+      {
+        source: "/used-car-dealers",
+        destination: "/dealers-used-cars",
+        permanent: true,
+      },
+      {
+        source: "/used-car-dealers/",
+        destination: "/dealers-used-cars",
+        permanent: true,
+      },
+      {
+        source: "/accident-car-dealers",
+        destination: "/dealers-accident",
+        permanent: true,
+      },
+      {
+        source: "/accident-car-dealers/",
+        destination: "/dealers-accident",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
